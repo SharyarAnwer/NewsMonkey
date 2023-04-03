ee668068345df2e0c8ccfb202ecce3d31b384a58
SharyarAnwer <sharyar.anwer0009@gmail.com>
2023-04-04 01:29:24 +0500

    I AM MAKING MY FIRST COMMIT

--- src/App.js
+++ src/App.js
@@ -21,7 +21,8 @@ const App = ()=> {
         color='#f11946'
         progress={progress} 
       />
-        <Switch>
+
+       {/*  <Switch>
           <Route exact path="/"><News setProgress={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country="gb" category="general"/></Route> 
           <Route exact path="/business"><News setProgress={setProgress} apiKey={apiKey} key="business" pageSize={pageSize} country="gb" category="business"/></Route> 
           <Route exact path="/entertainment"><News setProgress={setProgress} apiKey={apiKey} key="entertainment" pageSize={pageSize} country="gb" category="entertainment"/></Route> 
@@ -30,6 +31,24 @@ const App = ()=> {
           <Route exact path="/science"><News setProgress={setProgress} apiKey={apiKey} key="science" pageSize={pageSize} country="gb" category="science"/></Route> 
           <Route exact path="/sports"><News setProgress={setProgress} apiKey={apiKey} key="sports" pageSize={pageSize} country="gb" category="sports"/></Route> 
           <Route exact path="/technology"><News setProgress={setProgress} apiKey={apiKey} key="technology" pageSize={pageSize} country="gb" category="technology"/></Route> 
+        </Switch> */}
+
+        <Switch>
+          <Route exact path="/"><News setProgress={setProgress} apiKey="5ba9b87db59e4067a88148a43119fef2" key="general" pageSize={pageSize} country="gb" category="general"/></Route>
+
+          <Route exact path="/business"><News setProgress={setProgress} apiKey="5ba9b87db59e4067a88148a43119fef2" key="business" pageSize={pageSize} country="gb" category="business"/></Route> 
+
+          <Route exact path="/entertainment"><News setProgress={setProgress} apiKey="5ba9b87db59e4067a88148a43119fef2" key="entertainment" pageSize={pageSize} country="gb" category="entertainment"/></Route> 
+
+          <Route exact path="/general"><News setProgress={setProgress} apiKey="5ba9b87db59e4067a88148a43119fef2" key="general" pageSize={pageSize} country="gb" category="general"/></Route> 
+
+          <Route exact path="/health"><News setProgress={setProgress} apiKey="5ba9b87db59e4067a88148a43119fef2" key="health" pageSize={pageSize} country="gb" category="health"/></Route> 
+
+          <Route exact path="/science"><News setProgress={setProgress} apiKey="5ba9b87db59e4067a88148a43119fef2" key="science" pageSize={pageSize} country="gb" category="science"/></Route> 
+
+          <Route exact path="/sports"><News setProgress={setProgress} apiKey="5ba9b87db59e4067a88148a43119fef2" key="sports" pageSize={pageSize} country="gb" category="sports"/></Route> 
+
+          <Route exact path="/technology"><News setProgress={setProgress} apiKey="5ba9b87db59e4067a88148a43119fef2" key="technology" pageSize={pageSize} country="gb" category="technology"/></Route> 
         </Switch>
         </Router>
       </div>
